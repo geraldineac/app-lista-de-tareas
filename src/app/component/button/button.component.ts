2a7eb36bf7e0896276d2510cdaabfb0201fab5cc
geraldineac <87266809+geraldineac@users.noreply.github.com>
2022-09-19 08:36:51 -0300

    1 file modified

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -4,5 +4,7 @@ import { Component, VERSION } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
-  
 })
+export class AppComponent  {
+  
+}
