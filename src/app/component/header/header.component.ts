158ff8a3378211a35e4584d2d1689d7628870e79
geraldineac <87266809+geraldineac@users.noreply.github.com>
2022-09-20 11:45:13 -0300

    1 file modified

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class AppComponent {
+export class HeaderComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
 }
