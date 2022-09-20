010a4bece49ca96fce8600b0499b526185b93c59
geraldineac <87266809+geraldineac@users.noreply.github.com>
2022-09-20 11:43:12 -0300

    1 file modified

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
